976521b38435b27e400fd991db9dbd9e67ba6ce0
maneeshaindrachapa <maneesh.15@cse.mrt.ac.lk>
2018-01-29 23:04:52 +0530

    Bug fixes

--- src/keystroke/user.java
+++ src/keystroke/user.java
@@ -15,13 +15,17 @@ public class user {
     private String username;
     private String password;
     private ArrayList<String> timeElapsed=new ArrayList<>();
+    private boolean rightShifted=false;
+    private boolean leftShifted=false;
     
-    user(String username,String password,ArrayList<String> timeElapsed){
+    user(String username,String password,ArrayList<String> timeElapsed,boolean rightShifted,boolean leftShifted){
         this.username=username;
         this.password=password;
         for(int i=0;i<timeElapsed.size();i++){
             this.timeElapsed.add(timeElapsed.get(i));
         }
+        this.rightShifted=rightShifted;
+        this.leftShifted=leftShifted;
     }
 
     /**
@@ -43,5 +47,26 @@ public class user {
      */
     public ArrayList<String> getTimeElapsed() {
         return timeElapsed;
+    }
+
+    /**
+     * @return the rightShifted
+     */
+    public boolean isRightShifted() {
+        return rightShifted;
+    }
+
+    /**
+     * @param rightShifted the rightShifted to set
+     */
+    public void setRightShifted(boolean rightShifted) {
+        this.rightShifted = rightShifted;
+    }
+
+    /**
+     * @return the leftShifted
+     */
+    public boolean isLeftShifted() {
+        return leftShifted;
     }
 }
